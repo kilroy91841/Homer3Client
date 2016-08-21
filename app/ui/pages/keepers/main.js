38f8efccc8dd0755e574f4ed389d987c82004829
Ari Golub <arigolub@gmail.com>
2016-08-21 16:18:11 -0400

    keeper stuff

--- app/ui/pages/keepers/main.js
+++ app/ui/pages/keepers/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LoggedInComponent from 'ui/logged-in-component';
+import { connect } from 'react-redux';
 
 import { getKeepers, saveKeepers } from 'api/keepers';
 import { getFullTeam } from 'api/team';
@@ -8,70 +9,22 @@ import { teamId } from 'auth';
 import PlayerList from 'ui/player-list';
 
 import PlayerRow from 'ui/player-row';
+import KeeperRow from 'ui/pages/keepers/keeper-row';
 import Switch from 'react-bootstrap-switch';
 
-const KeeperRow = React.createClass({
-	render: function() {
-		var majorLeaguerSelected = this.props.keepers.majorLeaguers[this.props.player.id] != undefined;
-		var minorLeaguerSelected = this.props.keepers.minorLeaguers[this.props.player.id] != undefined;
+import { getNextSalary } from 'ui/pages/keepers/util';
 
-		var hasRookieStatus = this.props.player.currentSeason.hasRookieStatus;
-		var majorLeaguerCount = Object.keys(this.props.keepers.majorLeaguers).length;
-		var minorLeaguerCount = Object.keys(this.props.keepers.minorLeaguers).length;
-		var currentKeeperSeason = this.props.player.currentSeason.keeperSeason;
-		var noKeeperYearsLeft = currentKeeperSeason == 3;
-		var currentSalary = this.props.player.currentSeason.salary;
-		var majorLeaguerDisabled = (!majorLeaguerSelected && majorLeaguerCount >= 10) || (noKeeperYearsLeft);
-		var minorLeaguerDisabled = !minorLeaguerSelected && minorLeaguerCount >= 10;
-		return (
-			<PlayerRow player={this.props.player} 
-				className={noKeeperYearsLeft ? "bg-danger" : majorLeaguerSelected || minorLeaguerSelected ? "bg-success" : ""}>
-				<div className="col-md-1">
-					{this.props.player.position.name}
-				</div>
-				<div className={"col-md-2"}>
-					{this.props.player.name}
-				</div>
-				<div className="col-md-1 text-right">
-					{currentKeeperSeason}
-				</div>
-				<div className="col-md-1 text-right">
-					{currentSalary}
-				</div>
-				<div className="col-md-1 text-right">
-					{noKeeperYearsLeft ? "--" : currentKeeperSeason + 1}
-				</div>
-				<div className="col-md-1 text-right">
-					{noKeeperYearsLeft ? "--" : currentSalary + 3}
-				</div>
-				<div className="col-md-2">
-					<input type="checkbox" 
-						checked={ majorLeaguerSelected ? "checked" : null } 
-						disabled={ majorLeaguerDisabled ? "disabled" : null }
-						onChange={(cb)=>this.props.toggle(this.props.player.id, false, cb.target.checked)}
-					/>
-				</div>
-				<div className="col-md-2">
-					{
-						hasRookieStatus ?
-						<input type="checkbox" 
-							checked={ minorLeaguerSelected ? "checked" : null } 
-							disabled={ minorLeaguerDisabled ? "disabled" : null }
-							onChange={(cb)=>this.props.toggle(this.props.player.id, true, cb.target.checked)}
-						/>
-						:
-						null
-					}
-				</div>
-			</PlayerRow>
-		);
-	}
-})
+const stateToProps = function(state) {
+    return {
+        metadata: state.reducer.metadata
+    }
+};
 
 const Keepers = React.createClass({
 	getInitialState: function() {
 		return {
 			allPlayers: [],
+			availableSalary: undefined,
 			keepers: { majorLeaguers: {}, minorLeaguers: {} }
 		}
 	},
@@ -92,53 +45,76 @@ const Keepers = React.createClass({
 							response.data.disabledList,
 							response.data.bench,
 							response.data.minorLeaguers);
-			self.setState({ allPlayers : allPlayers })
-		}, function(error) {
-			console.log(error);
-		});
-		getKeepers(teamId(), function(response) {
-			var keepersList = response.data.data;
-			var keepers =  { majorLeaguers: [], minorLeaguers: [] }
-			keepersList.map(function(keeper) {
-				if (keeper.isMinorLeaguer) {
-					keepers.minorLeaguers[keeper.playerId] = keeper;
-				} else {
-					keepers.majorLeaguers[keeper.playerId] = keeper;
+			var availableSalary;
+			response.data.draftDollars.forEach(function(dd) {
+				if (dd.season == self.props.metadata.nextSeason && dd.draftDollarType.name == 'MLBAUCTION') {
+					availableSalary = dd.amount;
 				}
 			});
-			self.setState({ keepers : keepers })
+			self.setState({ allPlayers : allPlayers });
+
+			getKeepers(teamId(), function(response) {
+				var keepersList = response.data.data;
+				var keepers =  { majorLeaguers: [], minorLeaguers: [] }
+				keepersList.map(function(keeper) {
+					if (keeper.isMinorLeaguer) {
+						keepers.minorLeaguers[keeper.playerId] = keeper;
+					} else {
+						keepers.majorLeaguers[keeper.playerId] = keeper;
+						allPlayers.forEach(function(player) {
+							if (player.id == keeper.playerId) {
+								availableSalary -= getNextSalary(player);
+							}
+						});
+					}
+				});
+				self.setState({ keepers : keepers, availableSalary: availableSalary })
+			}, function(error) {
+				console.log(error);
+			});
 		}, function(error) {
 			console.log(error);
 		});
 	},
 	saveKeepers: function() {
 		var keepers = [];
-		Object.keys(this.state.keepers.minorLeaguers).forEach(function(playerId) {
-			keepers.push(this.state.keepers.minorLeaguers[playerId]);
+		var self = this;
+		Object.keys(self.state.keepers.minorLeaguers).forEach(function(playerId) {
+			keepers.push(self.state.keepers.minorLeaguers[playerId]);
 		});
-		Object.keys(this.state.keepers.majorLeaguers).forEach(function(playerId) {
-			keepers.push(this.state.keepers.majorLeaguers[playerId]);
+		Object.keys(self.state.keepers.majorLeaguers).forEach(function(playerId) {
+			keepers.push(self.state.keepers.majorLeaguers[playerId]);
 		});
 		saveKeepers(teamId(), keepers, function(response) {
-			alert(response.data.message);
+			alert("Keepers saved!");
 		}, function(error) {
 			console.log(errors);
 		});
 	},
-	toggleKeeper: function(playerId, isMinorLeaguer, selected) {
+	toggleKeeper: function(playerId, nextSalary, isMinorLeaguer, selected, undoPick) {
 		console.log(selected);
 		var keeper = { playerId: playerId, isMinorLeaguer: isMinorLeaguer };
 		var keepers = this.state.keepers;
 		delete keepers.minorLeaguers[playerId];
 		delete keepers.majorLeaguers[playerId];
+		var availableSalary = this.state.availableSalary;
+		debugger;
 		if (selected) {
 			if (isMinorLeaguer) {
 				keepers.minorLeaguers[playerId] = keeper;
 			} else {
+				if (availableSalary - nextSalary < 0) {
+					alert("Not enough funds to keep the selected player");
+					undoPick();
+					return;
+				}
 				keepers.majorLeaguers[playerId] = keeper;
+				availableSalary -= nextSalary;
 			}
+		} else {
+			availableSalary += nextSalary;
 		}
-		this.setState({ keepers: keepers });
+		this.setState({ keepers: keepers, availableSalary: availableSalary });
 	},
 	render: function() {
 		var self = this;
@@ -150,23 +126,54 @@ const Keepers = React.createClass({
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-md-6">
+					<div className="col-md-12">
 						<div className="row">
-							<div className="col-md-6">
-								Major League Keepers:
+							<div className="col-md-1">
+								<span className="h4">Rules</span>
 							</div>
-							<div className="col-md-6">
-								{Object.keys(this.state.keepers.majorLeaguers).length}
+							<div className="col-md-11">
+								<ul>
+									<li>Players can be kept at most 3 times. There is no limit on the numbers of times a player can be kept as a Minor Leaguer.</li>
+									<li>If a player does not have rookie eligibility (150 AB or 50 IP in a single season), they cannot be kept as a Minor Leaguer.</li>
+									<li>Players kept as Major Leaguers have a salary increase of 3 dollars. Players kept as Minor leaguers have no salary increase.</li>
+									<li>Undrafted players have a salary of 0. Traded players or players added as free agents retain their drafted salary.</li>
+									<li>Teams can keep 10 Major Leaguers and 10 Minor Leaguers</li>
+								</ul>
 							</div>
 						</div>
 						<div className="row">
 							<div className="col-md-6">
-								Minor League Keepers:
-							</div>
-							<div className="col-md-6">
-								{Object.keys(this.state.keepers.minorLeaguers).length}
+								<div className="row">
+									<div className="col-md-10">
+										<span className="h4">Major League Keepers:</span>
+									</div>
+									<div className="col-md-2">
+										<span className="h4">{Object.keys(this.state.keepers.majorLeaguers).length}</span>
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-md-10">
+										<span className="h4">Minor League Keepers:</span>
+									</div>
+									<div className="col-md-2">
+										<span className="h4">{Object.keys(this.state.keepers.minorLeaguers).length}</span>
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-md-10">
+										<span className="h4">{this.props.metadata.nextSeason} Remaining Available Dollars:</span>
+									</div>
+									<div className="col-md-2">
+										<span className="h4">{this.state.availableSalary}</span>
+									</div>
+								</div>
 							</div>
 						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-12">
+						<input type="button" value="SAVE KEEPERS" onClick={this.saveKeepers} />
 					</div>
 				</div>
 				<div className="row">
@@ -211,4 +218,4 @@ const Keepers = React.createClass({
 	}
 });
 
-export default Keepers;
+export default connect(stateToProps)(Keepers);
