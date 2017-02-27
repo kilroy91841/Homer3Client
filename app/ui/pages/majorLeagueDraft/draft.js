553f71ba18dc1e5aaacca0264e5e3e146a7801d8
Ari Golub <arigolub@gmail.com>
2017-02-26 23:59:45 -0500

    position filter; missing positions

--- app/ui/pages/majorLeagueDraft/draft.js
+++ app/ui/pages/majorLeagueDraft/draft.js
@@ -46,6 +46,13 @@ const Team = React.createClass({
 					{
 						this.props.data.players.map(function (player) {
 							return (
+								!player.name ?
+								<div className="row">
+									<div className="col-md-2">
+										{player.currentSeason.fantasyPosition.name}
+									</div>
+								</div>
+								:
 								<PlayerRow key={player.id} player={player}>
 									<div className="col-md-2">
 										{player.currentSeason.fantasyPosition.name}
@@ -119,15 +126,117 @@ const DraftHistory = React.createClass({
 	}
 });
 
+const DraftPlayerSearch = React.createClass({
+	getInitialState: function() {
+		return {
+			playerToDraft: {},
+			filterPositions: []
+		}
+	},
+	onPlayerToDraftChange: function(player) {
+		this.setState( { playerToDraft : player });
+	},
+	toggleFilterPosition: function(newFilterPosition) {
+		var filterPositions = this.state.filterPositions;
+		var contains = filterPositions.includes(newFilterPosition);
+		if (contains)
+		{
+			filterPositions = filterPositions.filter(function(f) {
+				return f != newFilterPosition;
+			});
+		}
+		else
+		{
+			filterPositions.push(newFilterPosition);
+		}
+		this.setState({filterPositions : filterPositions});
+	},
+	render: function() {
+		var self = this;
+		const players = this.props.players.filter(function(player) {
+			return self.state.filterPositions.length == 0 || self.state.filterPositions.includes(player.position.name);
+		});
+		players.forEach(function(player) {
+			var searchDisplay = player.name + " - " + player.position.name;
+			if (player.mlbTeam)
+			{
+				searchDisplay += " - " + player.mlbTeam.abbreviation;
+			}
+			player.searchDisplay = searchDisplay;
+		});
+		players.sort(function(a, b) {
+			if (a.lastName < b.lastName)
+			{
+				return -1;
+			}
+			else if (a.lastName > b.lastName)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		});
+		return (
+			<div className="row">
+				<div className="col-md-12">
+					<div className="row">
+						<div className="col-md-6 col-md-offset-4">
+							<label><input type="checkbox" value="C" onClick={()=>self.toggleFilterPosition("C")}/>C</label>
+							<label><input type="checkbox" value="1B" onClick={()=>self.toggleFilterPosition("1B")}/>1B</label>
+							<label><input type="checkbox" value="2B" onClick={()=>self.toggleFilterPosition("2B")}/>2B</label>
+							<label><input type="checkbox" value="3B" onClick={()=>self.toggleFilterPosition("3B")}/>3B</label>
+							<label><input type="checkbox" value="SS" onClick={()=>self.toggleFilterPosition("SS")}/>SS</label>
+							<label><input type="checkbox" value="OF" onClick={()=>self.toggleFilterPosition("OF")}/>OF</label>
+							<label><input type="checkbox" value="DH" onClick={()=>self.toggleFilterPosition("DH")}/>DH</label>
+							<label><input type="checkbox" value="P" onClick={()=>self.toggleFilterPosition("P")}/>P</label>
+							<label><input type="checkbox" value="RP" onClick={()=>self.toggleFilterPosition("RP")}/>RP</label>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-4">
+							<h3>Available Players</h3>
+						</div>
+						<div className="col-md-6">
+							<PlayerSearch
+								labelKey={"searchDisplay"}
+								value={this.state.playerToDraft}
+								onChange={this.onPlayerToDraftChange}
+								players={players}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+});
+
 const MajorLeagueDraft = React.createClass({
 	getInitialState: function() {
 		return {
 			freeAgents: [],
 			teams: [],
-			playerToDraft: {},
 			currentPlayer: {},
 			teamPlayers: [],
 			picks: []
+		}
+	},
+	addMissing: function(players, position, sort, needed) {
+		const missingCount = players.filter(function(p) {
+			return p.currentSeason.fantasyPosition.name == position;
+		}).length;
+		const toAdd = needed - missingCount;
+		if (toAdd > 0)
+		{
+			for (var i = 0; i < toAdd; i++)
+			{
+				const dummyPlayer = {};
+				dummyPlayer.currentSeason = {};
+				dummyPlayer.currentSeason.fantasyPosition = { name : position, sort : sort};
+				players.push(dummyPlayer);
+			}
 		}
 	},
 	getData: function() {
@@ -148,10 +257,21 @@ const MajorLeagueDraft = React.createClass({
 				self.props.teams.forEach(function(team) {
 					var teamView = {};
 					teamView.team = team;
-
 					var sortedPlayers = players.filter(function(player) {
 						return player.currentSeason.teamId == team.id;
 					});
+					
+					self.addMissing(sortedPlayers, "C", 1, 2);
+					self.addMissing(sortedPlayers, "1B", 2, 1);
+					self.addMissing(sortedPlayers, "2B", 3, 1);
+					self.addMissing(sortedPlayers, "3B", 4, 1);
+					self.addMissing(sortedPlayers, "SS", 5, 1);
+					self.addMissing(sortedPlayers, "2B/SS", 6, 1);
+					self.addMissing(sortedPlayers, "1B/3B", 7, 1);
+					self.addMissing(sortedPlayers, "OF", 8, 5);
+					self.addMissing(sortedPlayers, "U", 9, 1);
+					self.addMissing(sortedPlayers, "P", 10, 9);
+					
 					sortedPlayers.sort(function(a, b) {
 						if (a.currentSeason.fantasyPosition.sort < b.currentSeason.fantasyPosition.sort)
 						{
@@ -163,9 +283,36 @@ const MajorLeagueDraft = React.createClass({
 						}
 						else
 						{
-							return 0;
+							if (a.name && !b.name)
+							{
+								return -1;
+							}
+							else if (!a.name && b.name)
+							{
+								return 1;
+							}
+							else if (!a.name && b.name)
+							{
+								return 0;
+							}
+							else 
+							{
+								if (a.name > b.name)
+								{
+									return -1;
+								}
+								else if (a.name < b.name)
+								{
+									return 1;
+								}
+								else
+								{
+									return 0;
+								}
+							}
 						}
 					});
+
 					teamView.players = sortedPlayers;
 					teamView.draftDollar = draftDollars.filter(function(dd) {
 						return dd.teamId == team.id;
@@ -199,9 +346,6 @@ const MajorLeagueDraft = React.createClass({
 			self.getData();
 		}.bind(this), 5000);
 	},
-	onPlayerToDraftChange: function(player) {
-		this.setState( { playerToDraft : player });
-	},
 	render: function() {
 		return (
 			<div className="row">
@@ -216,18 +360,9 @@ const MajorLeagueDraft = React.createClass({
 							</PlayerRow>
 						</div>
 					</div>
-					<div className="row">
-						<div className="col-md-6">
-							<h3>Available Players</h3>
-						</div>
-						<div className="col-md-6">
-							<PlayerSearch
-								value={this.state.playerToDraft}
-								onChange={this.onPlayerToDraftChange}
-								players={this.state.freeAgents}
-							/>
-						</div>
-					</div>
+					<DraftPlayerSearch
+						players={this.state.freeAgents}
+					/>
 					<div className="row">
 						<div className="col-md-12">
 							<PlayerChangePosition players={this.state.teamPlayers}/>
